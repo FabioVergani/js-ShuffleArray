63cba9881a405eb5713fbbfe493dbaab68d72bbf
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-25 15:31:14 +0100

    Update shuffle-min.js

--- shuffle-min.js
+++ shuffle-min.js
@@ -1,1 +1,8 @@
-const shuffle=m=>{const f=Math.random;let i=m.length;while(i!==0){const a=m[--i],b=(f()*i)<<0;m[b]=m[i];m[i]=a};return m};
+					shuffled=x=>{
+						let i=x.length,m=[];
+						if(i!==0){
+							const f=w.Math.random,g=(v,i,m)=>{const a=m[i],b=(f()*i)<<0;m[b]=m[i];m[i]=a};
+							x.forEach(g)
+						};
+						return x
+					};
