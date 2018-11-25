d1b6059d552f8683d2af6d203a88b1953cd0e7bb
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-11-25 15:31:30 +0100

    Update shuffle.js

--- shuffle.js
+++ shuffle.js
@@ -1,12 +1,10 @@
-const shuffle=m=>{
-	const f=Math.random;
-	let i=m.length;
-	while(i!==0){
-		const a=m[--i],b=(f()*i)<<0;
-		m[b]=m[i];
-		m[i]=a
-	};
-	return m;
-};
+					shuffled=x=>{
+						let i=x.length,m=[];
+						if(i!==0){
+							const f=w.Math.random,g=(v,i,m)=>{const a=m[i],b=(f()*i)<<0;m[b]=m[i];m[i]=a};
+							x.forEach(g)
+						};
+						return x
+					};
 
 //console.log(shuffle([1,2,3,4,5,6,7,8,9,10,11,12]));
